8f8e68ce6a5a7e5e0d21fff17efd503bedc774eb
ahr <ahrasheed@gmail.com>
2025-09-04 10:56:53 +0200

    (fix) Fixes gpg key installation during reinstallation

--- install.sh
+++ install.sh
@@ -66,7 +66,7 @@ install_dependencies() {
             # Set up Charm repository specifically for gum
             info "Setting up Charm repository to install gum..."
             mkdir -p /etc/apt/keyrings
-            curl -fsSL https://repo.charm.sh/apt/gpg.key | gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+            curl -fsSL https://repo.charm.sh/apt/gpg.key | gpg --yes --dearmor -o /etc/apt/keyrings/charm.gpg
             echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | tee /etc/apt/sources.list.d/charm.list
 
             apt-get update
